de919b60539396ca8ea52c1693c9c8d5e0c5e0f3
Ryan Gupta <ryangupta8@gmail.com>
2020-07-10 15:58:48 -0500

    moving back to master

---
+++ laikago_ros/laikago_controller/src/test_joint_control.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "laikago_msgs/LowCmd.h"
+#include "laikago_msgs/LowState.h"
+#include "laikago_msgs/MotorCmd.h"
+#include "laikago_msgs/MotorState.h"
+#include <geometry_msgs/WrenchStamped.h>
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/Bool.h>
+#include <vector>
+#include <string.h>
+#include <math.h>
+#include <nav_msgs/Odometry.h>
+#include "laikago_gazebo/body.h"
+
+// using namespace laikago;
+
+
+int main(int argc, char *argv[])
+{
+	// laikago_controller::LaikagoJointController controller;
+	// ServoCmd command;
+
+ //    std::cout << command.mode << command.mode << '\n';
+}
